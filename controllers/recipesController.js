22efdc96c847642d9599c97d1eb26daa52626046
Nhlakanipho <nhlakaniphoradebe337@gmail.com>
2024-10-22 10:18:09 -1100

    Feature: endpoints created delete,put, and get with id's on database.js and recipeController

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -11,4 +11,55 @@ const createItem = async (req,res)=>{
     }
 }
 
-export default createItem;
+// endpoint to retrive recipes
+const findrecipe = async (req,res)=>{
+  try{
+    const recipe = await Recipe.find()
+    res.status(201).json(recipe)
+  } catch (error){
+    res.status(500).json({error:"an error occurred while fetching recipes",details: error.message})
+  }
+}
+
+// find recipe by id
+const findRecipeById = async (req,res)=>{
+  const {id} = req.params;
+  try{
+    const recipe = await Recipe.findById(id);
+    if (!recipe){
+      return res.status(404).json({error: "Recipe not found"})
+    }
+    res.status(201).json(recipe)
+  } catch (error){
+    res.status(500).json({error:"an error occurred while fetching a recipe",details: error.message,validate: "please enter a valid id"})
+  }
+}
+
+const deleteRecipeById = async (req,res)=>{
+  const {id} = req.params;
+  try{
+    const recipe = await Recipe.findByIdAndDelete(id);
+    if (!recipe){
+      return res.status(404).json({error: "Recipe not found"})
+    }
+    res.status(201).json({message:"recipe deleted successfully",recipe})
+  } catch (error){
+    res.status(500).json({error:"an error occurred while fetching a recipe",details: error.message,validate: "please enter a valid id"})
+  }
+}
+
+const updateRecipeById = async (req,res)=>{
+  const {id} = req.params;
+  const updateData = req.body;
+  try{
+    const recipe = await Recipe.findByIdAndUpdate(id, updateData, { new: true, runValidators: true });
+    if (!recipe){
+      return res.status(404).json({error: "Recipe not found"})
+    }
+    res.status(201).json({message:"recipe deleted successfully",recipe})
+  } catch (error){
+    res.status(500).json({error:"an error occurred while updating the recipe",details: error.message})
+  }
+}
+
+export  { createItem, findrecipe,findRecipeById,deleteRecipeById,updateRecipeById  };
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,22 @@
 import express from "express"
-import createItem from "../controllers/recipesController.js"
+import {createItem,findrecipe,findRecipeById,deleteRecipeById,updateRecipeById  } from "../controllers/recipesController.js"
 
 const router = express.Router()
 
+// Route for creating a new recipe
 router.post('/recipe', createItem)
+
+// Route for retrieving all recipes
+router.get('/recipe',findrecipe)
+
+// Route for retrieving a recipe by ID
+router.get('/recipe/:id',findRecipeById)
+
+// Route for deleting a recipe by ID
+router.delete("/recipe/:id",deleteRecipeById)
+
+// Route for updating a recipe by ID
+router.put("/recipe/:id",updateRecipeById )
+
 
 export default router
